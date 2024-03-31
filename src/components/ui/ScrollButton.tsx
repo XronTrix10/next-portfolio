c9a77f3296d5e13ebc08c274f7996d9e41604402
Xron Trix <xrontrix@gmail.com>
2024-03-31 09:39:57 +0530

    increased z index

--- src/components/ui/ScrollButton.tsx
+++ src/components/ui/ScrollButton.tsx
@@ -25,7 +25,7 @@ export default function ScrollButton() {
   }, [showScroll]);
 
   return (
-    <div className="fixed bottom-6 right-6 z-20" id="scrollbtn">
+    <div className="fixed bottom-6 right-6 z-50" id="scrollbtn">
       {showScroll && (
         <button
           onClick={scrollToTop}
