6ac64206208fb4541e3710ad91df6a3f814f6f72
Xron Trix <xrontrix@gmail.com>
2024-09-21 21:22:30 +0530

    feat: added new api route for mail sending

---
+++ src/app/api/mail/route.ts
@@ -0,0 +1,55 @@
+import { sendMail } from "@/src/lib/mail";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  let contact: string = "";
+  let message: string = "";
+  try {
+    const { contact: contactData, message: messageData } = await req.json();
+    contact = contactData;
+    message = messageData;
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Incorrect data format" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  if (!contact || !message) {
+    return new NextResponse(
+      JSON.stringify({ message: "Missing contact or message" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  const success = await sendMail({
+    to: "",
+    name: "",
+    subject: "Message from Portfolio",
+    body: `Contact: ${contact}\n\n\n${message}`,
+  });
+
+  if (!success) {
+    return new NextResponse(
+      JSON.stringify({ message: "Failed to send email" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  return new NextResponse(
+    JSON.stringify({ message: "Email sent successfully" }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+};
